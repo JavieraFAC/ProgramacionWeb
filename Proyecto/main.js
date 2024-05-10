107ae9d8cf85e87f622fcd9f05a1666021ba4b60
fevejar <149845244+fevejar@users.noreply.github.com>
2024-05-10 12:35:39 -0400

    Add files via upload
    corrección de error en formulario

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -49,3 +49,60 @@ nav.innerHTML= `
 `;
 
 
+
+
+var modalContent = `
+<div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-body">
+        <div class="container d-flex justify-content-center align-items-center">
+          <div class="card">
+            <div class="upper">
+              <img src="https://i.imgur.com/Qtrsrk5.jpg" class="img-fluid">
+            </div>
+            <div class="user text-center">
+              <div class="profile">
+                <img src="https://i.imgur.com/JgYD2nQ.jpg" class="rounded-circle" width="80">
+              </div>
+              <h4 class="mb-0">Juan Perez</h4>
+              <span class="text-muted d-block mb-2">Plan Semanal</span>
+              <button class="btn btn-primary btn-sm follow">Ver perfil</button>
+              <div class="d-flex justify-content-between align-items-center mt-4 px-4">
+                <div class="stats">
+                  <h6 class="mb-0">Almuerzos</h6>
+                  <span>7</span>
+                </div>
+                <div class="stats">
+                  <h6 class="mb-0">Saldo</h6>
+                  <span>$14.123</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <!-- Podría usarse un botón para cerrar el modal -->
+        <!--<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>-->
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+// Agrega el contenido del modal al contenedor
+document.getElementById('modal-container').innerHTML = modalContent;
+
+
+function iniciarMap(){
+  var coord = {lat:-33.0336892,lng: -71.535759};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 15,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
