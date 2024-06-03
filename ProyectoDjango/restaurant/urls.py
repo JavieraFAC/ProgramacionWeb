3ffea28495e46f1d2188e0db7b763746ee141a25
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2024-06-03 09:00:21 -0400

    Add files via upload

---
+++ ProyectoDjango/restaurant/urls.py
@@ -0,0 +1,17 @@
+#from django.conf.urls.import url
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.Index, name='Index'),
+    path('Register', views.Register, name='Register'),
+    path('Carrito', views.Carrito, name='Carrito'),
+    path('Productos', views.Productos, name='Productos'),
+    path('Cuenta', views.Cuenta, name='Cuenta'),
+    path('Planes', views.Planes, name='Planes'),
+    path('Formulario', views.Formulario, name='Formulario'),
+    path('Retaurantes', views.Retaurantes, name='Retaurantes'),
+    
+]
---
+++ ProyectoDjango/restaurant/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+from .models import Alumno, Genero
+# Create your views here.
+
+def Index(request):
+    context={}
+    return render(request, 'restaurant/Index.html', context)
+
+def Register(request):
+    context={}
+    return render(request, 'restaurant/Register.html', context)
+
+def Carrito(request):
+    context={}
+    return render(request, 'restaurant/Carrito.html', context)
+
+def Productos(request):
+    context={}
+    return render(request, 'restaurant/Productos.html', context)
+
+def Cuenta(request):
+    context={}
+    return render(request, 'restaurant/Cuenta.html', context)
+
+def Planes(request):
+    context={}
+    return render(request, 'restaurant/Planes.html', context)
+
+def Formulario(request):
+    context={}
+    return render(request, 'restaurant/Formulario.html', context)
+
+def Retaurantes(request):
+    context={}
+    return render(request, 'restaurant/Retaurantes.html', context)
